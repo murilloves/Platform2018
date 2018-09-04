f1b744e4d3f4d03bb70fa7286b3850c717c3ba03
murilloves <murillo.vieiraesilva@gmail.com>
2018-09-04 15:47:32 -0300

    Game Creation

---
+++ Assets/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 5f;
+	public float jumpingForce = 300f;
+	public SpriteRenderer spriteRenderer;
+
+	private bool canJump = false;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Going right
+		if (Input.GetKey("right")) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+			spriteRenderer.flipX = false;
+		}
+
+		// Going Left
+		if (Input.GetKey("left")) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+			spriteRenderer.flipX = true;
+		}
+
+		// Jumping
+		if (Input.GetKeyDown("space") && canJump) {
+			GetComponent<Rigidbody>().AddForce(0, jumpingForce, 0);
+			canJump = false;
+		}
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		if (collision.transform.name == "Ground") {
+			canJump = true;
+		}
+	}
+}
